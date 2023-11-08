9cb88345908e2c877650e19371d0a48906b19133
oridaniels1 <62208203+oridaniels1@users.noreply.github.com>
2023-11-08 11:41:14 +0000

    Update command_output_to_file.sh

--- Ansible/command_output_to_file.sh
+++ Ansible/command_output_to_file.sh
@@ -1,31 +1,51 @@
 #!/bin/bash
 
-# Function to execute the command and send output to a file
-execute_command() {
-    command=$1
-    output_file=$2
-
-# Execute the command and redirect the output to the file
-    $command > "$output_file"
-
-    echo "Command output saved to $output_file"
+# Function to display the usage of the script
+display_usage() {
+    echo "Usage: $0 [-c <command>] [-n <notes>] [-o <output_file>]"
+    echo "Options:"
+    echo "  -c <command>        Specify the command to execute"
+    echo "  -n <notes>          Add notes before the output"
+    echo "  -o <output_file>    Specify the output file (default: output.txt)"
 }
 
-# Main script
-# Check if the command option is provided
-if [[ $1 == "-c" ]]; then
-# Check if both the command and output file options are provided
-    if [[ -n $2 && -n $3 ]]; then
-        command_to_execute=$2
-        output_file_path=$3
+# Default values
+output_file="output.txt"
+command=""
+notes=""
 
-# Call the function to execute the command and send output to the file
-        execute_command "$command_to_execute" "$output_file_path"
-    else
-        echo "Error: Both command and output file options are required."
-        echo "Usage: bash script.sh -c 'command' output_file.txt"
-    fi
-else
-    echo "Error: Invalid option. Please use the -c option to specify the command."
-    echo "Usage: bash script.sh -c 'command' output_file.txt"
+# Parse command-line arguments
+while getopts ":c:n:o:" option; do
+    case "${option}" in
+        c)
+            command=${OPTARG}
+            ;;
+        n)
+            notes=${OPTARG}
+            ;;
+        o)
+            output_file=${OPTARG}
+            ;;
+        *)
+            display_usage
+            exit 1
+            ;;
+    esac
+done
+
+# Check if the mandatory command is provided
+if [[ -z $command ]]; then
+    echo "Error: Command is required."
+    display_usage
+    exit 1
 fi
+
+# Add notes to the output file
+if [[ -n $notes ]]; then
+    echo "$notes" >> "$output_file"
+fi
+
+# Execute the command and append output to the file
+eval "$command" >> "$output_file"
+
+echo "Output written to $output_file"
